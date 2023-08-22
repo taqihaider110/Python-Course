f06d00be471e0c3a4b0d67e4d63ff73eeed7c50c
Taqi haider <taqihaider591@gmail.com>
2023-08-22 19:59:37 +0500

    Operator and Operand

--- tut2.py
+++ tut2.py
@@ -1,9 +1,23 @@
 
-def taqi(x,y):
-    print(y,x)
+# def taqi(x,y):
+#     print(y,x)
 
 
-x=5
-y= "taqi"
+# x=5
+# y= "taqi"
 
-taqi(x,y)
+# taqi(x,y)
+a=int(input("Enter English Marks: "))
+b=int(input("Enter Physics Marks:"))
+c=int(input("Enter Maths Marks:"))
+d=int(input("Enter Chemistry Marks:"))
+e=int(input("Enter Urdu Marks:"))
+sum = a+b+c+d+e
+# sum =5060
+percentage = (sum/500)*100
+print(percentage)
+print("English",a)
+print("Physics",b)
+print("Maths",c)
+print("Chemistry",d)
+print("Urdu",e)
