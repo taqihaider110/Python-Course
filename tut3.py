6d63776e474636a3e2b32a8e6ba7e18f9c9c0283
Taqi haider <taqihaider591@gmail.com>
2023-08-23 01:17:18 +0500

    Function Calls

--- tut3.py
+++ tut3.py
@@ -2,9 +2,9 @@ def square(num):
     return num ** 2
 
 print(square(3))
-def sub(num1,num2):
-    return num1-num2
 
-sub(3,1)
+def sub(num1, num2):
+    return num1 - num2
 
-print(sub(square(num1),square(num2+1)))
+result = sub(square(3), square(1 + 1))
+print(result)
