46c12d31668b3658c4c1f5facc1808f7623c5dd3
Taqi haider <taqihaider591@gmail.com>
2023-08-22 20:08:40 +0500

    Operator and Operands

--- tut2.py
+++ tut2.py
@@ -7,17 +7,24 @@
 # y= "taqi"
 
 # taqi(x,y)
-a=int(input("Enter English Marks: "))
-b=int(input("Enter Physics Marks:"))
-c=int(input("Enter Maths Marks:"))
-d=int(input("Enter Chemistry Marks:"))
-e=int(input("Enter Urdu Marks:"))
-sum = a+b+c+d+e
+# a=int(input("Enter English Marks: "))
+# b=int(input("Enter Physics Marks:"))
+# c=int(input("Enter Maths Marks:"))
+# d=int(input("Enter Chemistry Marks:"))
+# e=int(input("Enter Urdu Marks:"))
+# sum = a+b+c+d+e
 # sum =5060
-percentage = (sum/500)*100
-print(percentage)
-print("English",a)
-print("Physics",b)
-print("Maths",c)
-print("Chemistry",d)
-print("Urdu",e)
+# percentage = (sum/500)*100
+# print(percentage)
+# print("English",a)
+# print("Physics",b)
+# print("Maths",c)
+# print("Chemistry",d)
+# print("Urdu",e)
+print(5+2)
+print(5-2)
+print(5*2)
+print(10/2)
+print(10//2)
+print(10%2)
+print(10**2)
