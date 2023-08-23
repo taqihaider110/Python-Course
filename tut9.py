dff9a6907925c843415ab6f364c8b93cc0159efd
Taqi haider <taqihaider591@gmail.com>
2023-08-23 21:30:36 +0500

    Updating variables

---
+++ tut9.py
@@ -0,0 +1,32 @@
+x = 6        # initialize x
+print(x)
+x = x + 1    # update x
+print(x)
+
+x = 6        # initialize x
+print(x)
+x += 3       # increment x by 3; same as x = x + 3
+print(x)
+x -= 1       # decrement x by 1
+print(x)
+
+s = 1
+print(s)
+s = s + 2
+print(s)
+s = s + 3
+print(s)
+s = s + 4
+print(s)
+s = s + 5
+print(s)
+s = s + 6
+print(s)
+s = s + 7
+print(s)
+s = s + 8
+print(s)
+s = s + 9
+print(s)
+s = s + 10
+print(s)
