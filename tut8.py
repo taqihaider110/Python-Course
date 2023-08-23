8d7f974e5aabca2d7f69e7deced96eee073eceb4
Taqi haider <taqihaider591@gmail.com>
2023-08-23 20:37:44 +0500

    Statement & Expression

--- tut8.py
+++ tut8.py
@@ -1,3 +1,5 @@
+import math
+
 print(2 * len("hello") + len("goodbye"))
 y = 3.14
 x = len("hello")
@@ -12,7 +14,22 @@ def square(x):
     return x ** 2
 def sub(x,y):
     return x-y
+def sum(x,y):
+    return x+y
+    
 
 print(square(y + 3))
 print(square(y + square(x)))
 print(sub(square(y), square(x)))
+
+x=min(5,10,15)
+print(x)
+b=max(25,100,23)
+print(b)
+y=math.sqrt(25)
+print(y)
+z=sub(5,6)
+print(z)
+a=pow(5,2)
+print(a)
+print(sum(5,6))
