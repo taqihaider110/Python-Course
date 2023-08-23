c5a8246108b6710c8efb4879824ea7b65d53a0d7
Taqi haider <taqihaider591@gmail.com>
2023-08-23 20:05:29 +0500

    Operators

---
+++ tut7.py
@@ -0,0 +1,10 @@
+bruce = 5
+print(bruce)
+bruce = 7
+print(bruce)
+
+a = 5
+b = a    # after executing this line, a and b are now equal
+print(a,b)
+a = 3    # after executing this line, a and b are no longer equal
+print(a,b)
