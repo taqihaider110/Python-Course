ef70d6cc80ff1a67d55778051a7a65984b730824
Taqi haider <taqihaider591@gmail.com>
2023-08-23 20:20:23 +0500

    Statement and Expresssion

---
+++ tut8.py
@@ -0,0 +1,18 @@
+print(2 * len("hello") + len("goodbye"))
+y = 3.14
+x = len("hello")
+print(x)
+print(y)
+
+print(2 * len("hello") + len("goodbye"))
+
+x = 2
+y = 1
+def square(x):
+    return x ** 2
+def sub(x,y):
+    return x-y
+
+print(square(y + 3))
+print(square(y + square(x)))
+print(sub(square(y), square(x)))
