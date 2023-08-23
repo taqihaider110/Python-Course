50926c41a89c96d5681a1f6cc04d9d719dbcdb84
Taqi haider <taqihaider591@gmail.com>
2023-08-23 19:59:44 +0500

    Variables

---
+++ tut6.py
@@ -0,0 +1,15 @@
+message = "What's up, Doc?"
+n = 17
+pi = 3.14159
+
+print(message)
+print(n)
+print(pi)
+
+message = "What's up, Doc?"
+n = 17
+pi = 3.14159
+
+print(type(message))
+print(type(n))
+print(type(pi))
