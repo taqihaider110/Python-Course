1f823ef5e4c138d40767daa5cf116d5fbd5f8528
Taqi haider <taqihaider591@gmail.com>
2023-08-27 03:22:34 +0500

    Practice of turtle

---
+++ tut16.py
@@ -0,0 +1,12 @@
+import turtle
+wn = turtle.Screen()
+
+elan = turtle.Turtle()
+
+distance = 50
+for _ in range(10):
+    elan.forward(distance)
+    elan.right(90)
+    distance = distance + 10
+
+wn.exitonclick()
