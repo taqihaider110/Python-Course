c69ca938952b6209dff7d45708dd9567653967de
Taqi haider <taqihaider591@gmail.com>
2023-08-27 03:17:58 +0500

    For loop

---
+++ tut15.py
@@ -0,0 +1,7 @@
+print("This will execute first")
+
+for _ in range(3):
+    print("This line will execute three times")
+    print("This line will also execute three times")
+
+print("Now we are outside of the for loop!")
