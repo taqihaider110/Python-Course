ec5ffd0d83ef12641da8e34b6986e79707955735
Taqi haider <taqihaider591@gmail.com>
2023-08-27 03:31:18 +0500

    push

--- tut17.py
+++ tut17.py
@@ -4,12 +4,24 @@ wn.bgcolor("light blue")
 taqi=turtle.Turtle()
 taqi.pensize(5)
 taqi.color("red")
-distance=50
+distance=30
 angle=45
 
 for _ in range(10):
     taqi.forward(distance)
     taqi.right(angle)
+    angle-=10
+    
+wasi=turtle.Turtle()
+wasi.pensize(10)
+wasi.color("Green")
+distance=30
+angle=45
+wasi.left(180)
+wasi.forward(distance)
+for _ in range(10):
+    wasi.forward(distance)
+    wasi.left(angle)
     angle-=10
 
 wn.exitonclick()
