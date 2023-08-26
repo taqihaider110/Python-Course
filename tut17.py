e12add96bf04a7a2aab19dda58f680ea9d02c589
Taqi haider <taqihaider591@gmail.com>
2023-08-27 03:23:38 +0500

    Repetition of Turtle with For loop

---
+++ tut17.py
@@ -0,0 +1,15 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("light blue")
+taqi=turtle.Turtle()
+taqi.pensize(5)
+taqi.color("red")
+distance=50
+angle=45
+
+for _ in range(10):
+    taqi.forward(distance)
+    taqi.right(angle)
+    angle+=10
+
+wn.exitonclick()
