d2dcec98955f93bab81dc41d0855b6e38ccd8dcb
Taqi haider <taqihaider591@gmail.com>
2023-08-27 05:07:46 +0500

    Random Module

--- tut21.py
+++ tut21.py
@@ -1,7 +1,13 @@
-import random
+# import random
+from random import random, randrange
+# prob = random.random()
+# print(prob)
 
-prob = random.random()
+# diceThrow = random.randrange(1,7)       # return an int, one of 1,2,3,4,5,6
+# print(diceThrow)
+
+prob = random()
 print(prob)
 
-diceThrow = random.randrange(1,7)       # return an int, one of 1,2,3,4,5,6
+diceThrow = randrange(1,7)       # return an int, one of 1,2,3,4,5,6
 print(diceThrow)
