0545f3aec24ea602b50b4848b447c5b4b36c9a39
Taqi haider <taqihaider591@gmail.com>
2023-09-14 02:39:58 +0500

    Slicing

---
+++ tut30.py
@@ -0,0 +1,14 @@
+singers = "Peter, Paul, and Mary"
+print(singers[0:5])
+print(singers[7:11])
+print(singers[17:21])
+
+fruit = "banana"
+print(fruit[:3])
+print(fruit[3:])
+
+a_list = ['a', 'b', 'c', 'd', 'e', 'f']
+print(a_list[1:3])
+print(a_list[:4])
+print(a_list[3:])
+print(a_list[:])
