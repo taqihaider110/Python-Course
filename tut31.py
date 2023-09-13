9bbddba8eab3e2cc5f3f2d34763c8084ce972eee
Taqi haider <taqihaider591@gmail.com>
2023-09-14 02:49:52 +0500

    Slicing

---
+++ tut31.py
@@ -0,0 +1,23 @@
+# Creating a List
+List = ['G', 'E', 'E', 'K', 'S', 'F',
+		'O', 'R', 'G', 'E', 'E', 'K', 'S']
+print("Initial List: ")
+print(List)
+
+# Print elements from beginning
+# to a pre-defined point using Slice
+Sliced_List = List[:-6]
+print("\nElements sliced till 6th element from last: ")
+print(Sliced_List)
+
+# Print elements of a range
+# using negative index List slicing
+Sliced_List = List[-6:-1]
+print("\nElements sliced from index -6 to -1")
+print(Sliced_List)
+
+# Printing elements in reverse
+# using Slice operation
+Sliced_List = List[::-1]
+print("\nPrinting List in reverse: ")
+print(Sliced_List)
