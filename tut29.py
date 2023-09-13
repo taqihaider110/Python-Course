e1efbe84472856a8772ff6280cdfcba3ec1bb61b
Taqi haider <taqihaider591@gmail.com>
2023-09-14 02:33:48 +0500

    List Slicing

---
+++ tut29.py
@@ -0,0 +1,8 @@
+taqi=["hello","naiq",1,"wasi","ali","asad"]
+print(taqi[2])
+taqi.append(5)
+print(taqi)
+print(taqi[:2])
+print(len(taqi))
+taqi = taqi[3:] + ["Eat","play",4] +taqi[:2]
+print(taqi)
