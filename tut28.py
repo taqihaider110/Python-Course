f3c5dfbd1d3106d12714f0f9502a35da78ab700e
Taqi haider <taqihaider591@gmail.com>
2023-09-14 02:24:24 +0500

    Indexing of list

---
+++ tut28.py
@@ -0,0 +1,11 @@
+school = "Luther College"
+m = school[2]
+print(m)
+
+lastchar = school[-1]
+print(lastchar)
+
+numbers = [17, 123, 87, 34, 66, 8398, 44]
+print(numbers[2])
+print(numbers[9-8])
+print(numbers[-2])
