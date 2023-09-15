5f6acb227c60e2c8e72cd577ff40653d213ae990
Taqi haider <taqihaider591@gmail.com>
2023-09-15 16:04:59 +0500

    push

---
+++ tut40.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for n in range(5):
+    print(n, fruits[n])
+
+print("New Code")
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for n in range(len(fruits)):
+    print(n, fruits[n])
+
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for fruit in fruits:
+    print(fruit)
