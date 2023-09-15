6656731032e6be8a790191093a68d12ffedcaaa6
Taqi haider <taqihaider591@gmail.com>
2023-09-15 15:24:03 +0500

    Split and join

--- tut36.py
+++ tut36.py
@@ -4,3 +4,8 @@ z = "".join(x)
 y = z.split()
 a = "".join(y)
 print(a)
+
+# Using split() and join() methods
+# The split() method will break the given string by the specified separator
+# and return a list of strings. The str separator is used by the join() 
+# method to connect the elements of a sequence into a string.
