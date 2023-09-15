6a1be64e57f92f0914170e97977242b4a0ad9793
Taqi haider <taqihaider591@gmail.com>
2023-09-15 17:58:00 +0500

    Boolean Expression

---
+++ tut43.py
@@ -0,0 +1,14 @@
+print(True)
+print(type(True))
+print(type(False))
+
+print(5 == 5)
+print(5 == 6)
+
+x=5;y=3
+print(x != y)               # x is not equal to y
+
+print(x > y)                # x is greater than y
+print(x < y)                # x is less than y
+print(x >= y)               # x is greater than or equal to y
+print(x <= y)               # x is less than or equal to y
