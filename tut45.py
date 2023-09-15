4bc6b94355c8dab996e2398f84399846649fda33
Taqi haider <taqihaider591@gmail.com>
2023-09-15 19:30:32 +0500

    The in and not in Operator

---
+++ tut45.py
@@ -0,0 +1,13 @@
+print('p' in 'apple')
+print('i' in 'apple')
+print('ap' in 'apple')
+print('pa' in 'apple')
+
+print('a' in 'a')
+print('apple' in 'apple')
+print('' in 'a')
+print('' in 'apple')
+
+print("a" in ["a", "b", "c", "d"])
+print(9 in [3, 2, 9, 10, 9.0])
+print('wow' not in ['gee wiz', 'gosh golly', 'wow', 'amazing'])
