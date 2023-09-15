0c015233e3daf1c194faf455d05a16870024b68a
Taqi haider <taqihaider591@gmail.com>
2023-09-15 14:45:14 +0500

    Count function

---
+++ tut33.py
@@ -0,0 +1,9 @@
+a = "I have had an apple on my desk before!"
+print(a.count("e"))
+print(a.count("ha"))
+
+z = ['atoms', 4, 'neutron', 6, 'proton', 4, 'electron', 4, 'electron', 'atoms']
+print(z.count("4"))
+print(z.count(4))
+print(z.count("a"))
+print(z.count("electron"))
