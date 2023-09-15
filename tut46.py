8e9f09c33f21d693ca920c609f3b80d4d55a5ec9
Taqi haider <taqihaider591@gmail.com>
2023-09-15 20:29:18 +0500

    If-Else

--- tut46.py
+++ tut46.py
@@ -1,8 +1,19 @@
-x = 15
-
+x = 15; y=3
+ 
 if x % 2 == 0:
     print(x, "is even")
     print("This is Even")
 else:
     print(x, "is odd")
     print("This is Odd")
+
+x = 10
+y = 10
+
+if x < y:
+    print("x is less than y")
+else:
+    if x > y:
+        print("x is greater than y")
+    else:
+        print("x and y must be equal")
