df566010c0f59d1fc02f0356cb7f22600c06d4c2
Taqi haider <taqihaider591@gmail.com>
2023-09-15 20:32:53 +0500

    Control Statement

--- tut46.py
+++ tut46.py
@@ -17,3 +17,13 @@ else:
         print("x is greater than y")
     else:
         print("x and y must be equal")
+
+x = 10
+y = 10
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x and y must be equal")
