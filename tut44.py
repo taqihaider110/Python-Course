299fc34e8c8fc1b7ebd9873a6f3e88e4b0d69516
Taqi haider <taqihaider591@gmail.com>
2023-09-15 19:15:44 +0500

    Logical operators

--- tut44.py
+++ tut44.py
@@ -9,3 +9,11 @@ print(x > 0 and x < 10)
 
 n = 25
 print(n % 2 == 0 or n % 3 == 0)
+
+total_weight = int(input('Enter total weight of luggage:'))
+num_pieces = int(input('Number of pieces of luggage?'))
+
+if total_weight / num_pieces > 50:
+   print('Average weight is greater than 50 pounds -> $100 surcharge.')
+
+print('Luggage check complete.')
