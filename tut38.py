fbfd2e081bf55e9c54feb18c8773f1bb7a3d63b4
Taqi haider <taqihaider591@gmail.com>
2023-09-15 15:54:37 +0500

    For loop

---
+++ tut38.py
@@ -0,0 +1,19 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+accum = 0
+for w in nums:
+    accum = accum + w
+print(accum)
+
+accum = 0
+for w in range(11):
+    accum = accum + w
+print(accum)
+
+# or, if you use two inputs for the range function
+
+sec_accum = 0
+for w in range(1,11):
+    sec_accum = sec_accum + w
+print(sec_accum)
+
+numbers = list(range(53))
