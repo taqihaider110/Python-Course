d0ccac7a60f22b8c3d26216f0ad00176de014da5
Taqi haider <taqihaider591@gmail.com>
2023-09-15 15:35:51 +0500

    For loop

---
+++ tut37.py
@@ -0,0 +1,10 @@
+for name in ["Joe", "Amy", "Brad", "Angelina", "Zuki", "Thandi", "Paris"]:
+    print("Hi", name, "Please come to my party on Saturday!")
+
+for achar in "Go Spot Go":
+    print(achar)
+
+fruits = ["apple", "orange", "banana", "cherry"]
+
+for afruit in fruits:     # by item
+    print(afruit)
