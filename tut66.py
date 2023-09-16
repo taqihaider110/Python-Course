be7600bb6ea1ea1a8ae6fb929621753d9ea753fe
Taqi haider <taqihaider591@gmail.com>
2023-09-17 01:01:17 +0500

    practice

---
+++ tut66.py
@@ -0,0 +1,8 @@
+# For each character in the string saved in ael, append that character to a list 
+# that should be saved in a variable app.
+
+ael = "python!"
+app=[]
+for i in ael:
+    app.append(i)
+print(app)
