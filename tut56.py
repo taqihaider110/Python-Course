61503797df76d973004224ad3e6fdb48712d2b60
Taqi haider <taqihaider591@gmail.com>
2023-09-16 12:56:17 +0500

    Objects and Reference

---
+++ tut56.py
@@ -0,0 +1,20 @@
+a = "banana"
+b = "banana"
+
+print(a is b)
+
+a = "banana"
+b = "banana"
+
+print(id(a))
+print(id(b))
+
+a = [81,82,83]
+b = [81,82,83]
+
+print(a is b)
+
+print(a == b)
+
+print(id(a))
+print(id(b))
