4c80087e2176811a4a5861a62bba46341b901e9b
Taqi haider <taqihaider591@gmail.com>
2023-09-23 01:40:00 +0500

    ReadFile

--- tut73.py
+++ tut73.py
@@ -1,5 +1,7 @@
 fileref = open("olympics.txt", "r")
 ## other code here that refers to variable fileref
-for i in fileref:
-    print(i.strip())
+f=fileref.read()
+# for i in fileref:
+    # print(i.strip())
+print(len(f))
 fileref.close()
